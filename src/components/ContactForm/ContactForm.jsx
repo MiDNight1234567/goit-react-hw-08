b7bf63f17f3c8457062cb9d60c79293089516b2e
Данил <danikkach666@gmail.com>
2024-04-25 21:08:19 +0300

    add

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -55,7 +55,7 @@ const ContactForm = () => {
           <span className={css.labelText}>Name</span>
           <Field
             className={css.formField}
-            placeholder="Anna Dubkova"
+            placeholder="Marina Dubkova"
             type="text"
             name="name"
           />
